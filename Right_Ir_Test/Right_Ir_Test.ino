c959fa7be4dc72a85c72fc3ff4df4d936629bc07
CodingM00se <62729855+CodingM00se@users.noreply.github.com>
2021-11-03 19:45:13 -0400

    Add files via upload

---
+++ Right_Ir_Test/Right_Ir_Test.ino
@@ -0,0 +1,27 @@
+/**
+ * Right IR sensor test
+ * Eric McCallum
+ */
+
+
+void setup() {
+  tone(4, 3000, 1000); // play speaker tone on pin 4, 3000 Hz for 1 s
+  delay(1000);
+  pinMode(3, INPUT);
+  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int irLeft = irDetect(2, 3, 38000);
+  Serial.println(irLeft);
+  delay(100);
+}
+
+int irDetect(int irLedPin, int irReceiverPin, long frequency) {
+  tone(irLedPin, frequency, 8); // IRLED 38 kHz for 1 ms
+  delay(1);
+  int ir = digitalRead(irReceiverPin);
+  delay(1); // down time before recheck (sleep period)
+  return ir; // return 1 not detect, 0 detect
+}
