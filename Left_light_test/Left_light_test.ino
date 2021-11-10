c501eb996a1419b10cb6fb7c11af6945f7f8cb52
CodingM00se <62729855+CodingM00se@users.noreply.github.com>
2021-11-10 18:41:40 -0500

    Update Left_light_test.ino

--- Left_light_test/Left_light_test.ino
+++ Left_light_test/Left_light_test.ino
@@ -5,11 +5,29 @@
 
 
 void setup() {
+  tone(4, 3000, 1000);
+  delay(1000);
+
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print("Light value left sensor = ");
-  Serial.println(analogRead(A0));
+  long tRight = rcTime(6);
+  Serial.print("tRight = ");
+  Serial.print(tRight);
+  Serial.println(" us");
   delay(1000);
+}
+
+long rcTime(int pin) {
+  pinMode(pin, OUTPUT); // charge capacitor
+  digitalWrite(pin, HIGH); // ... by setting pin output to high
+  delay(1); // for 1 ms
+  pinMode(pin, INPUT); // set pin to input
+  digitalWrite(pin, LOW); // with no pullup
+  long time = micros(); // mark the time
+
+  while(digitalRead(pin)); // wait for voltage < threshold 
+  time = micros() - time; // calculate decay time
+  return time; // return decay time 
 }
