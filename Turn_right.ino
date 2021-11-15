f370d77c2e3be2900784b7a7e81dfeb4a9e1aa8f
CodingM00se <62729855+CodingM00se@users.noreply.github.com>
2021-11-15 18:30:03 -0500

    Add files via upload

---
+++ Turn_right.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+Servo servoLeft;
+Servo servoRight;
+
+void setup() {
+  servoLeft.attach(13);
+  servoRight.attach(12);
+}
+
+void loop() {
+  servoLeft.writeMicroseconds(1300);
+  servoRight.writeMicroseconds(1300); 
+}
