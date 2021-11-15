6e1a1fa475444a6234f7fd83d67155da7b7fb51f
CodingM00se <62729855+CodingM00se@users.noreply.github.com>
2021-11-15 18:28:49 -0500

    Update Move_forward.ino

--- Move_forward/Move_forward.ino
+++ Move_forward/Move_forward.ino
@@ -3,14 +3,11 @@ Servo servoLeft;
 Servo servoRight;
 
 void setup() {
-  tone(4, 3000, 1000);
-  delay(1000);
-
   servoLeft.attach(13);
   servoRight.attach(12);
 }
 
 void loop() {
   servoLeft.writeMicroseconds(1700);
-  servoRight.writeMicroseconds(1300); // is moving faster despite being at max speed 
+  servoRight.writeMicroseconds(1300); 
 }
